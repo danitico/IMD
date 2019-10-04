5ad2955d7c06072e3edf0e4c1726d0399c785e14
Daniel <danitico98@gmail.com>
2019-10-04 19:00:47 +0200

    ejercicio5 done

---
+++ P1/ejercicio5.py
@@ -0,0 +1,45 @@
+#
+# El formato del fichero será el siguiente:
+# En la primera línea, separado por un espacio
+# se pondrá el número de filas y columnas
+# Cada fila del fichero será una fila de la matriz
+# Cada elemento estará separado por un espacio
+#
+import numpy as np
+import sys
+
+try:
+    f = open(sys.argv[1], 'rt')
+except IOError:
+    print("No se puede leer el fichero")
+    sys.exit(-1)
+
+lines = f.read().splitlines()
+
+shape = lines[0].split(' ')
+shape = [int(i) for i in shape]
+
+matrix = np.zeros(shape=(shape[0], shape[1]))
+
+for i in range(0, len(lines)-1):
+    lines[i+1] = lines[i+1].split(' ')
+    lines[i+1] = [int(j) for j in lines[i+1]]
+
+    matrix[i,:] = np.array(lines[i+1])
+
+print("La matriz es:")
+print(matrix)
+
+if np.linalg.det(matrix) == 0:
+    print("La matriz no es invertible")
+else:
+    print("La inversa de la matriz es:")
+    inversa = np.linalg.inv(matrix)
+    print(inversa)
+
+    #La matriz original multiplicada por la matriz⁻¹ tiene
+    #que dar la matriz identidad
+
+    print("Si la inversa se ha calculado correctamente")
+    print("La proxima matriz es la identidad de orden ", shape[0])
+    print(np.matmul(matrix, inversa).round(0))
