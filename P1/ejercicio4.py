572cab0c298429d87771d545b9d30ce3f5eb4e4a
Daniel <danitico98@gmail.com>
2019-10-04 17:54:17 +0200

    ejercicio4 done

---
+++ P1/ejercicio4.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy import stats
+
+try:
+    rows = int(input("Introduzca el número de filas: "))
+    cols = int(input("Introduzca el número de columnas: "))
+except ValueError:
+    print("No es un número")
+    exit(-1)
+
+matrix = np.zeros(shape=(rows, cols))
+
+for i in range(0, rows):
+    for j in range(0, cols):
+        try:
+            print("matrix[", i, "][", j, "] = ", end='')
+            matrix[i, j] = int(input())
+        except ValueError:
+            print("No es un numero")
+            exit(-1)
+
+print(matrix)
+
+print("La moda de la matriz es", stats.mode(matrix, axis=None)[0][0])
+print("La media de la matriz es ", np.mean(matrix))
