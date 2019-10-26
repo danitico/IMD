f265b802682a48543a0f709910cbd425d75de881
Daniel <danitico98@gmail.com>
2019-10-26 13:07:15 +0200

    results!

--- P3/classifiers.py
+++ P3/classifiers.py
@@ -10,11 +10,11 @@ import numpy as np
 import warnings
 
 
-# warnings.filterwarnings('ignore')
+warnings.filterwarnings('ignore')
 
 to_classify = ['data/BreastTissue.csv', 'data/contact-lenses.arff', 'data/diabetes.arff', 'data/ecoli.arff',
                'data/glass.arff', 'data/ionosphere.arff', 'data/divorce.arff', 'data/vote.arff', 'data/zoo.arff',
-               'segment.arff']
+               'data/segment.arff']
 
 treeaccuracy = list()
 knnaccuracy = list()
@@ -28,7 +28,12 @@ for i in to_classify:
         arffile = arff.load(open(i))
         data = np.array(arffile['data'])
 
-    data[data == None] = np.nan
+    if i == 'data/vote.arff':
+        data[data == None] = np.nan
+        imp = SimpleImputer(strategy='most_frequent')
+        imp.fit(data)
+        data = imp.transform(data)
+
     X = np.array(data[:, 0:data.shape[1]-1])
     X = X.astype(np.float64)
     Y = np.array(data[:, data.shape[1]-1])
@@ -51,16 +56,16 @@ for i in to_classify:
     print("accuracy Score = ", accuracy2.mean())
     knnaccuracy.append(accuracy2.mean())
 
-    # print("SVM")
-    # clf2 = svm.SVC(kernel='linear', C=1, random_state=42)
-    # accuracy3 = cross_val_score(clf2, X, Y, cv=10,
-    #                          scoring='accuracy')
-    #
-    # print("accuracy Score = ", accuracy3.mean())
-    # knnaccuracy.append(accuracy3.mean())
+    print("SVM")
+    clf2 = svm.SVC(kernel='linear', C=1, random_state=42)
+    accuracy3 = cross_val_score(clf2, X, Y, cv=10,
+                             scoring='accuracy')
 
-print(accuracy1)
-print(accuracy2)
-print(accuracy3)
+    print("accuracy Score = ", accuracy3.mean())
+    svmaccuracy.append(accuracy3.mean())
+
+print(treeaccuracy)
+print(knnaccuracy)
+print(svmaccuracy)
 
 
